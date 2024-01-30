836385ffc28c6810c9b95f83b0292983efe812e0
AdrianC167 <ac232126110@gmail.com>
2024-01-30 09:20:56 -0500

    initialized flask

---
+++ main.py
@@ -0,0 +1,10 @@
+import pymysql
+from flask import Flask
+from flask import render_template
+
+app = Flask(__name__)
+
+
+@app.route('/home')
+def index():
+    return render_template("index.html.jinja")
