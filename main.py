bfade5d6d8559fabad3fea2cd7e259b5804fba32
AdrianC167 <ac232126110@gmail.com>
2024-01-31 09:58:51 -0500

    created a landing page

--- main.py
+++ main.py
@@ -1,10 +1,9 @@
-import pymysql
 from flask import Flask
 from flask import render_template
 
 app = Flask(__name__)
 
 
-@app.route('/home')
+@app.route('/')
 def index():
     return render_template("index.html.jinja")
