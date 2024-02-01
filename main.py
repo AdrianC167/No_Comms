a2ce3a371d9889d41484112ea8145242478e4611
AdrianC167 <ac232126110@gmail.com>
2024-02-01 10:55:20 -0500

    almost finished with completing the user sign up

--- main.py
+++ main.py
@@ -1,9 +1,37 @@
 from flask import Flask
-from flask import render_template
+from flask import render_template,request, redirect
+import pymysql
+import pymysql.cursors
 
 app = Flask(__name__)
 
+conn = pymysql.connect(
+        database = "achen_no_comms",
+        user = "achen",
+        password = "232126110",
+        host = "10.100.33.60",
+        cursorclass = pymysql.cursors.DictCursor
+ )
 
 @app.route('/')
 def index():
     return render_template("index.html.jinja")
+
+@app.route('/signup', methods=['GET','POST'])
+def signup():
+
+    if request.method == 'POST':
+        email = request.form["Email"]
+        password = request.form["Password"]
+        name = request.form["Name"]
+        bio = request.form["Bio"]
+        username = request.form["Username"]
+        pronoun = request.form["pronouns"]
+        birthday = request.form["birthday"]
+        cursor = conn.cursor()
+        cursor.execute(f"INSERT INTO `users`(`Email` , `Password`, `Name`, `User_Bio`,`Pronoun`,`Birthday`) VALUES('{email}', '{email}', '{email}')")         
+        cursor.close()
+        conn.commit()
+    
+
+    return render_template('signup.html.jinja')
